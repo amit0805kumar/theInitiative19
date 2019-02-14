9c514198c3e0d9e06299aff989b141235fb50f35
Amit <amit0805kumar@gmail.com>
2019-02-14 20:55:36 +0530

    login ui made

---
+++ login/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var login = $('#loginBtn');
+    var signUp = $('#signUpBtn');
+    var loginForm = $('.loginForm');
+    var registerFrom = $('.registerFrom');
+    login.click(function(){
+        login.addClass('btnActive');
+        signUp.removeClass('btnActive');
+        loginForm.removeClass('hide');
+        registerFrom.addClass('hide');
+    });
+    signUp.click(function(){
+        signUp.addClass('btnActive');
+        login.removeClass('btnActive');
+        loginForm.addClass('hide');
+        registerFrom.removeClass('hide');
+    });
+    
+    
+
+});
