2edb63c182e95e1311daec8b8761c86198ef0424
Amit <amit0805kumar@gmail.com>
2019-02-15 17:31:34 +0530

    Profile made except home

--- profile/js/script.js
+++ profile/js/script.js
@@ -1,32 +1,32 @@
-$(function(){
+$(function () {
 
-var home = $('#home');
-var register = $('#register');
-var task = $('#task');
-var setting = $('#setting');
-var coordinators = $('#coordinators');
-    
-    
-    
-    home.click(function(){
+    var home = $('#home');
+    var register = $('#register');
+    var task = $('#task');
+    var coordinators = $('#coordinators');
+    var instructions = $('#instructions');
+
+
+
+    home.click(function () {
         $('.content__box').addClass('hide');
         $('#b1').removeClass('hide');
     });
-     register.click(function(){
+    register.click(function () {
         $('.content__box').addClass('hide');
         $('#b2').removeClass('hide');
     });
-     task.click(function(){
+    task.click(function () {
         $('.content__box').addClass('hide');
         $('#b3').removeClass('hide');
     });
-     setting.click(function(){
+    coordinators.click(function () {
         $('.content__box').addClass('hide');
         $('#b4').removeClass('hide');
-    });
-     coordinators.click(function(){
+    }); 
+    instructions.click(function () {
         $('.content__box').addClass('hide');
         $('#b5').removeClass('hide');
     });
-    
+
 });
