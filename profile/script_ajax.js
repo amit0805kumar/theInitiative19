94d799299a2a306c4f1cc4576c86f14076589007
Shashwat Vats <shashwatvats10@gmail.com>
2019-02-18 17:10:57 +0530

    synopsis/optonal task uploading

---
+++ profile/script_ajax.js
@@ -0,0 +1,103 @@
+$(document).ready(function () {
+
+    $(document).on('submit', '#upload_synopsis_form', function (event) {
+        console.log("vhgvgh");
+       // $('.loader').css('display','block');
+        event.preventDefault();
+        //$("#signup_submit").attr('disabled','disabled');
+        $.ajax({
+            url: "upload_synopsis.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //$("#signup_submit").removeAttr('disabled'); 
+
+            }
+        });
+
+
+    });
+
+    $(document).on('submit', '#task1_form', function (event) {
+        console.log("vhgvgh");
+       // $('.loader').css('display','block');
+        event.preventDefault();
+        //$("#signup_submit").attr('disabled','disabled');
+        $.ajax({
+            url: "upload_optional_task.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //$("#signup_submit").removeAttr('disabled'); 
+
+            }
+        });
+
+
+    });
+
+    $(document).on('submit', '#task2_form', function (event) {
+        console.log("vhgvgh");
+       // $('.loader').css('display','block');
+        event.preventDefault();
+        //$("#signup_submit").attr('disabled','disabled');
+        $.ajax({
+            url: "upload_optional_task.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //$("#signup_submit").removeAttr('disabled'); 
+
+            }
+        });
+
+
+    });
+
+    $(document).on('submit', '#task3_form', function (event) {
+        console.log("vhgvgh");
+       // $('.loader').css('display','block');
+        event.preventDefault();
+        //$("#signup_submit").attr('disabled','disabled');
+        $.ajax({
+            url: "upload_optional_task.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //$("#signup_submit").removeAttr('disabled'); 
+
+            }
+        });
+
+
+    });
+
+    $(document).on('submit', '#task4_form', function (event) {
+        console.log("vhgvgh");
+       // $('.loader').css('display','block');
+        event.preventDefault();
+        //$("#signup_submit").attr('disabled','disabled');
+        $.ajax({
+            url: "upload_optional_task.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //$("#signup_submit").removeAttr('disabled'); 
+
+            }
+        });
+
+
+    });
+
+});
